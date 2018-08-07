48b6fbeff9ce7c836c3b62ef2520bc03e2129bae
Thomas Tang <ahmost29@gmail.com>
2018-08-07 15:20:15 -0700

    GameStates - added update and render functions

--- src/GameStates.cpp
+++ src/GameStates.cpp
@@ -27,33 +27,52 @@ void MenuState::onExit()
 {
     std::cout << "Exit Menu" << std::endl;
 }
+void MenuState::update() {
+
+}
+void MenuState::render() {
+
+}
+
 
 PlayState::~PlayState() {}
-void PlayState::onEnter()
-{
+void PlayState::onEnter() {
     std::cout << "Enter Play" << std::endl;
 }
-void PlayState::onExit()
-{
+void PlayState::onExit() {
     std::cout << "Exit Play" << std::endl;
+}
+void PlayState::update() {
+
+}
+void PlayState::render() {
+
 }
 
 PauseState::~PauseState() {}
-void PauseState::onEnter()
-{
+void PauseState::onEnter() {
     std::cout << "Enter Pause" << std::endl;
 }
-void PauseState::onExit()
-{
+void PauseState::onExit() {
     std::cout << "Exit Pause" << std::endl;
+}
+void PauseState::update() {
+
+}
+void PauseState::render() {
+
 }
 
 OptionsState::~OptionsState() {}
-void OptionsState::onEnter()
-{
+void OptionsState::onEnter() {
     std::cout << "Enter Options" << std::endl;
 }
-void OptionsState::onExit()
-{
+void OptionsState::onExit() {
     std::cout << "Exit Options" << std::endl;
+}
+void OptionsState::update() {
+
+}
+void OptionsState::render() {
+
 }
